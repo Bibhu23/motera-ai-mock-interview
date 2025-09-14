7825deba93a79b7e8f22dcc33a131285483d5316
bibhujit <bibhujit@example.com>
2025-09-14 09:35:46 +0530

    Added simple Express server with /api/ping route

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Motera AI Mock Interview" 
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+app.listen(PORT, () => {
+    console.log(`server run on port ${PORT}`);
+})
