fff1c25935a1367a46da8cb9bea3b7c9f4d0b747
Bibhu23 <131533164+Bibhu23@users.noreply.github.com>
2025-10-10 16:18:41 +0530

    Update LiveVideoInterviewPage.js

--- frontend/src/pages/LiveVideoInterviewPage.js
+++ frontend/src/pages/LiveVideoInterviewPage.js
@@ -2,7 +2,7 @@ import { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./LiveVideoInterviewPage.css";
-import * as faceapi from "face-api.js/dist/face-api.min.js";
+import * as faceapi from "face-api.js";
 
 function LiveVideoInterviewPage() {
   const videoRef = useRef(null);
