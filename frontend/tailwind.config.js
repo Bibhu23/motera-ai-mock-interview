9407c0e010e88924eda4dbf393ce7bd645ae1593
Asit kumar Baral <165561420+Asitkumarbaral@users.noreply.github.com>
2025-10-10 15:24:39 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*/.{js,jsx,ts,tsx}",  // 👈 All React files
+    "./src/**/*.{js,jsx,ts,tsx}", // make sure it matches your file structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
